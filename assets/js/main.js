4a017029aa8b0df4d9a4735ea56190ecf1b098a6
Raffa Santos <atomix.luke@gmail.com>
2025-08-26 11:29:52 -0300

    testes para implemetacao da automatizacao de tags no html

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,34 @@ const offset = 0;
 const limit = 10;
 const url =`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+function convertPokemonToLi(pokemons){
+    return `
+        <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemons.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemons.name}">
+                </div>
+            </li>
+    `
+}
+
+
+        const pokemonList = document.getElementById('pokemonList')
+        pokemonList.appendChild('')
+
+// fetch(url)
+//     .then((response) => response.json())
+//     .then((jasonBody) => jasonBody.results)
+//     .then((pokemonList) => {
+//         for (let index = 0; index < pokemonList.length; index++) {
+//             const pokemon = pokemonList[index];
+//             console.log(convertPokemonToLi(pokemon))
+            
+//         }
+//     })
+//    .catch((error) => console.error(error))
