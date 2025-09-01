dffc4004a8e0b675969d0f595a47bc8967482bc7
Raffa Santos <atomix.luke@gmail.com>
2025-09-01 00:17:46 -0300

    usando agora promises para acessar os detalhes dos pokemons e alimentar os lis de forma mais detalhada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,18 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemons){
     return `
         <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemons.order}</span>
                 <span class="name">${pokemons.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${convertPokemonTypesToLi(pokemons.types).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemons.name}">
+                    <img src="${pokemons.sprites.other.dream_world.front_default}" alt="${pokemons.name}">
                 </div>
             </li>
     `
@@ -20,5 +22,5 @@ const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
     pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-    
+      
 })
