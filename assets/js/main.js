2c0657f66a7b8892e1f10ed205fc431f6e596315
Raffa Santos <atomix.luke@gmail.com>
2025-09-06 12:29:08 -0300

    estilização nos cards e agora a transferencia de pagina esta funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ let offset = 0;
 function loadPokemonItens(offset, limit) { 
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         pokemonList.innerHTML += pokemons.map((pokemon => `
-            <li class="pokemon ${pokemon.type}">
+            <li class="pokemonCard pokemon ${pokemon.type}" data-id="${pokemon.number}">
                     <span class="number">#${pokemon.number}</span>
                     <span class="name">${pokemon.name}</span>
                     <div class="detail">
@@ -21,6 +21,14 @@ function loadPokemonItens(offset, limit) {
             `)).join('')
     })
 }
+
+pokemonList.addEventListener('click', (event) => {
+    const card = event.target.closest('.pokemonCard')
+    if (card) {
+        const id = card.dataset.id
+        window.location.href = `pokemonSelect.html?id=${id}`
+    }
+})
 
 loadPokemonItens(offset, limit)
 
